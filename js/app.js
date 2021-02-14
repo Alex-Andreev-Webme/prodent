57cb564fe3669ad9255c31d0bbd0c90d152af3eb
Alex Andreev <webme.corporate@gmail.com>
2021-02-14 21:48:42 +0300

    feat: add dynamic active class for links

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const popupForm = document.querySelector('.popup__form');
 const contactButtons = document.querySelectorAll('.contact-button');
 const closePopupButton = document.querySelector('.popup__close-btn');
 const popupInputs = document.querySelectorAll('.popup__input');
+const pricesLinks = document.querySelectorAll('.prices__link');
 
 // Открыть попап
 function showPopup() {
@@ -21,8 +22,16 @@ function popupHandler(event) {
 	closePopup();
 }
 
+// Добавляем активный класс ссылкам
+function getActivePrices(event) {
+	pricesLinks.forEach(link => link.classList.remove('prices__link_active'));
+	event.target.classList.add('prices__link_active');
+}
+
 contactButtons.forEach(button => button.addEventListener('click', showPopup));
 
 closePopupButton.addEventListener('click', closePopup);
 
 popupForm.addEventListener('submit', popupHandler);
+
+pricesLinks.forEach(link => link.addEventListener('click', getActivePrices));
